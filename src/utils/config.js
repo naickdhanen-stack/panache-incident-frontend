c28f9c88b70b49964e24009c694a273c382e5495
Naik VENGIDASAMY <naik@panache.mu>
2025-11-12 10:10:55 +0400

    Update code with latest changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
-export const API_URL = 'http://localhost:5000/api';
+// src/config.js
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export const SOURCES_OF_INCIDENT = [
   'Customer Complaint',
